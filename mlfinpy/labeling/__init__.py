2accd64b9e24ab1ee6bd460fab72ee306579ea27
baobach <95854748+baobach@users.noreply.github.com>
2024-10-08 12:58:15 +0700

    build: update internal API of triple_barrier method

--- mlfinpy/labeling/__init__.py
+++ mlfinpy/labeling/__init__.py
@@ -5,17 +5,17 @@ Labeling techniques used in financial machine learning.
 from mlfinpy.labeling.fixed_time_horizon import fixed_time_horizon
 from mlfinpy.labeling.labeling import (
     add_vertical_barrier,
-    apply_pt_sl_on_t1,
     barrier_touched,
     drop_labels,
     get_bins,
     get_events,
+    triple_barriers,
 )
 from mlfinpy.labeling.raw_return import raw_return
 
 __all__ = [
     "add_vertical_barrier",
-    "apply_pt_sl_on_t1",
+    "triple_barriers",
     "barrier_touched",
     "drop_labels",
     "get_bins",
